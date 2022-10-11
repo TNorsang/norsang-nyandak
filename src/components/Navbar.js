75e470d3dad03f99881de9f33e6b955f6808e1ea
git config --global user.name TNorsang <thenorsang@gmail.com>
2022-10-11 17:43:55 -0400

    testing secondary branch

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import '../styles/Navbar.css';
 function Navbar(){
     return (
         <div className="Navbar">
-            <p>Norsang Nyandak</p>
+            <p>Norsang Nyandak testing</p>
         </div>
     );
 }
