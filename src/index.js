03a945385aed0ab98213a9a85d802f584a33785e
git config --global user.name TNorsang <thenorsang@gmail.com>
2022-10-12 17:25:30 -0400

    Added some changes

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-// Index.js 
-// This is the structure of the whole website
+// Index.js is the structure of the whole website
+
+
+import '../src/styles/Index.css'
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import Background from './components/Background';
-import Navbar from './components/Navbar';
-import '../src/styles/Index.css'
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
     <div id="main">
-        {/* <Navbar /> */}
         <Background />
-        {/* <App /> */}
     </div>
    
 );
