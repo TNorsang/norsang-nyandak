aebc513b6a8437e7c99373d7be174e735814713c
git config --global user.name TNorsang <thenorsang@gmail.com>
2022-10-12 16:39:59 -0400

    Finished all the movements of background icons

--- src/components/Background.js
+++ src/components/Background.js
@@ -41,24 +41,45 @@ function Background(){
             <div class="x11">
                 <FontAwesomeIcon icon={solid('robot')} class="icons" id="robot"/>
             </div>
-                        
-                        
-                        
-                        
-                        <FontAwesomeIcon icon={brands('square-js')} class="icons" id="square-js"/>
-                        <FontAwesomeIcon icon={brands('java')} class="icons" id="java"/>
-                        <FontAwesomeIcon icon={solid('laptop-code')} class="icons" id="laptop-code"/>
-                        <FontAwesomeIcon icon={brands('stack-overflow')} class="icons" id="stack-overflow"/>
-                        <FontAwesomeIcon icon={brands('html5')} class="icons" id="html5"/>
-                        <FontAwesomeIcon icon={solid('dumbbell')} class="icons" id="dumbbell"/>
-                        <FontAwesomeIcon icon={brands('react')} class="icons" id="react"/>
-                        <FontAwesomeIcon icon={solid('yin-yang')} class="icons" id="yin-yang"/>
-                        <FontAwesomeIcon icon={solid('dog')} class="icons" id="dog"/>
-                        <FontAwesomeIcon icon={solid('cat')} class="icons" id="cat"/>
-                        <FontAwesomeIcon icon={solid('earth-americas')} class="icons" id="earth-americas"/>
-                        <FontAwesomeIcon icon={solid('fish')} class="icons" id="fish"/>
-                        <FontAwesomeIcon icon={brands('google')} class="icons" id="google" />
-            
+            <div class="x11">
+                <FontAwesomeIcon icon={brands('square-js')} class="icons" id="square-js"/>
+            </div>
+            <div class="x12">
+                <FontAwesomeIcon icon={brands('java')} class="icons" id="java"/>
+            </div>
+            <div class="x13">
+                <FontAwesomeIcon icon={solid('laptop-code')} class="icons" id="laptop-code"/>
+            </div>
+            <div class="x14">
+                <FontAwesomeIcon icon={brands('stack-overflow')} class="icons" id="stack-overflow"/>
+            </div>
+            <div class="x15">
+                <FontAwesomeIcon icon={brands('html5')} class="icons" id="html5"/>
+            </div>
+            <div class="x16">
+                <FontAwesomeIcon icon={solid('dumbbell')} class="icons" id="dumbbell"/>
+            </div>
+            <div class="x17">
+                <FontAwesomeIcon icon={brands('react')} class="icons" id="react"/>
+            </div>
+            <div class="x18">
+                <FontAwesomeIcon icon={solid('yin-yang')} class="icons" id="yin-yang"/>
+            </div>
+            <div class="x19">
+                <FontAwesomeIcon icon={solid('dog')} class="icons" id="dog"/>
+            </div>
+            <div class="x20">
+                <FontAwesomeIcon icon={solid('cat')} class="icons" id="cat"/>
+            </div>
+            <div class="x21">
+                <FontAwesomeIcon icon={solid('earth-americas')} class="icons" id="earth-americas"/>
+            </div>
+            <div class="x22">
+                <FontAwesomeIcon icon={solid('fish')} class="icons" id="fish"/>
+            </div>
+            <div class="x23">
+                <FontAwesomeIcon icon={brands('google')} class="icons" id="google" />
+            </div>
         </div>
     );
 }
