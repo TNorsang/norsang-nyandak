c481b47479b06b8e042fafdac984a886bce27957
Norsang Nyandak <TNorsang>
2022-10-28 19:10:01 -0400

    fixed the width issue

--- src/components/Abouts.js
+++ src/components/Abouts.js
@@ -1,26 +1,36 @@
-import ReactDOM from 'react-dom';
-import '../styles/Abouts.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import 'font-awesome/css/font-awesome.min.css';
-import { solid, regular, brands, icon } from '@fortawesome/fontawesome-svg-core/import.macro';
-import React, { Component } from 'react';
+import ReactDOM from "react-dom";
+import "../styles/Abouts.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "font-awesome/css/font-awesome.min.css";
+import {
+  solid,
+  regular,
+  brands,
+  icon,
+} from "@fortawesome/fontawesome-svg-core/import.macro";
+import React, { Component } from "react";
 
 function Abouts() {
-    return (
-        <div className="Abouts">
-            <h1>
-  <span>always be</span>
-  <div class="message">
-    <div class="word1">close</div>
-    <div class="word2">code</div>
-    <div class="word3">creating</div>
-  </div>
-</h1>
-            <div><FontAwesomeIcon icon={regular('hand')} id="hand" /></div>     
-            <div id="bio"><p>My name is Norsang Nyandak.</p></div>
-        </div>
-    );
+  return (
+    <div className="Abouts">
+      <div id="hello">
+        <h1>Hello</h1>
+      </div>
+      <div>
+        <FontAwesomeIcon icon={regular("hand")} id="hand" />
+      </div>
+      <div>
+        <p>
+          My name is Norsang Nyandak.
+        </p>
+      </div>
+      {/* <h1 id="hello">Hello</h1>
+        <FontAwesomeIcon icon={regular("hand")} id="hand" />
+      <div id="bio">
+        <p>My name is Norsang Nyandak.</p>
+      </div> */}
+    </div>
+  );
 }
-
 
 export default Abouts;
