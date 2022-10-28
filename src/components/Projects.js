591cf7708ac8d0de3856dafef262299e53e43e19
git config --global user.name TNorsang <thenorsang@gmail.com>
2022-10-27 22:26:46 -0400

    adding icons in about me

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,22 +2,42 @@ import ReactDOM from 'react-dom';
 import '../styles/Projects.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import 'font-awesome/css/font-awesome.min.css';
-import { solid, regular, brands, icon } from '@fortawesome/fontawesome-svg-core/import.macro'; 
-import React, { Component }  from 'react';
+import { solid, regular, brands, icon } from '@fortawesome/fontawesome-svg-core/import.macro';
+import React, { Component } from 'react';
 
-function Projects(){
-    return(
+function Projects() {
+    return (
         <div className='Container'>
             <div className="Projects">
-                <div id="lexical">
-                    Lexical Analyzer
+                <span id="text">About Me</span>
+                <div class="circles">
+                    <a href="https://github.com/TNorsang" target="_blank" >
+                        <FontAwesomeIcon icon={brands('github')} id="git" class="git" />
+                    </a>
                 </div>
-                <div>
-                    <p id="text">Projects</p>
+                <div class="circles">
+                    <a href="https://github.com/TNorsang" target="_blank" >
+                        <FontAwesomeIcon icon={brands('github')} id="git1" class="git" />
+                    </a>
                 </div>
+                <div class="circles">
+                    <a href="https://github.com/TNorsang" target="_blank" >
+                        <FontAwesomeIcon icon={brands('github')} id="git2" class="git" />
+                    </a>
+                </div>
+                <div class="circles">
+                    <a href="https://github.com/TNorsang" target="_blank" >
+                        <FontAwesomeIcon icon={brands('github')} id="git3" class="git" />
+                    </a>
+                </div>
+                <div class="circles">
+                    <a href="https://github.com/TNorsang" target="_blank" >
+                        <FontAwesomeIcon icon={brands('github')} id="git4" class="git" />
+                    </a>
+                </div>
+
             </div>
         </div>
-        
     );
 }
 
