b305769cfdc55664ba18678eefb7d16226bf8c1c
git config --global user.name TNorsang <thenorsang@gmail.com>
2022-12-07 01:02:41 -0500

    put icons in wave class

--- src/components/Abouts.js
+++ src/components/Abouts.js
@@ -13,25 +13,15 @@ import React, { Component } from "react";
 function Abouts() {
   return (
     <div className="Abouts">
-      <div className="gitLinkTwit">
-        <a href="https://github.com/TNorsang" target="_blank" className="glt">
-          <FontAwesomeIcon icon={brands("github")} id="gitH" className="pageIcon" />
-        </a>
-        <a href="https://www.linkedin.com/in/norsang-nyandak-077577173/" target="_blank" className="glt">
-          <FontAwesomeIcon icon={brands("linkedin")} id="linked" className="pageIcon" />
-        </a>
-      </div>
-      <div className="line"></div>
-
-      <div className="card" tabindex="0">
-        <span className="card__infoicon">
-          {/* <i className="fa fa-info"></i> */}
-        </span>
-        <h1 className="card__title">Hello and Welcome!</h1>
-        <p className="card__description">Hello and welcome to my portfolio!</p>
-      </div>
-
       <div className="handsWaveContainer">
+        <div className="gitLinkTwit">
+          <a href="https://github.com/TNorsang" target="_blank" className="glt">
+            <FontAwesomeIcon icon={brands("github")} id="gitH" className="pageIcon" />
+          </a>
+          <a href="https://www.linkedin.com/in/norsang-nyandak-077577173/" target="_blank" className="glt">
+            <FontAwesomeIcon icon={brands("linkedin")} id="linked" className="pageIcon" />
+          </a>
+        </div>
         <div id="hello">
           <h1>Hello</h1>
         </div>
