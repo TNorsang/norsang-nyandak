e861547f73b36583aca792f844571d93d2a41c08
Norsang Nyandak <TNorsang>
2024-03-21 19:28:00 -0400

    updated about me page

--- src/components/Abouts.js
+++ src/components/Abouts.js
@@ -1,43 +1,87 @@
-import ReactDOM from "react-dom";
-import "../styles/Abouts.css";
+import { motion } from "framer-motion";
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "font-awesome/css/font-awesome.min.css";
 import {
-  solid,
-  regular,
   brands,
-  icon,
+  regular,
 } from "@fortawesome/fontawesome-svg-core/import.macro";
-import React, { Component } from "react";
+import "../styles/Abouts.css";
+
 // import Media from 'react-media';
 
 function Abouts() {
   return (
     <div className="Abouts">
-      <div className="handsWaveContainer">
-        <div className="gitLinkTwit">
-          <a href="https://github.com/TNorsang" target="_blank" className="glt">
-            <FontAwesomeIcon icon={brands("github")} id="gitH" className="pageIcon" />
-          </a>
-          <a href="https://www.linkedin.com/in/norsang-nyandak-077577173/" target="_blank" className="glt">
-            <FontAwesomeIcon icon={brands("linkedin")} id="linked" className="pageIcon" />
-          </a>
-        </div>
-        <div id="hello">
-          <h1>Hello</h1>
-        </div>
-        <div className="hand">
-          <FontAwesomeIcon icon={regular("hand")} id="hand" />
+      <div className="helloContainer">
+        <motion.div
+          className="handsWaveContainer"
+          initial={{ opacity: 0, y: -100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+          <div className="gitLinkTwit">
+            <a
+              href="https://github.com/TNorsang"
+              target="_blank"
+              className="glt"
+            >
+              <FontAwesomeIcon
+                icon={brands("github")}
+                id="gitH"
+                className="pageIcon"
+              />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/norsang-nyandak-077577173/"
+              target="_blank"
+              className="glt"
+            >
+              <FontAwesomeIcon
+                icon={brands("linkedin")}
+                id="linked"
+                className="pageIcon"
+              />
+            </a>
+          </div>
+          <div id="hello">
+            <h1>Hello</h1>
+          </div>
+          <motion.div
+            className="hand"
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
+            transition={{ duration: 0.5, delay: 0.5 }}
+          >
+            <FontAwesomeIcon icon={regular("hand")} id="hand" />
+          </motion.div>
+        </motion.div>
+      </div>
+      <div className="imageContainer">
+        <motion.div
+          drag // Enables dragging
+          dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }} // Constrains dragging within the parent container
+          dragElastic={0.5} // Adjusts the "bounciness" of dragging
+          style={{
+            width: 200,
+            height: 200,
+            backgroundColor: "blue",
+            borderRadius: 20,
+            cursor: "grab", // Change cursor to indicate draggable content
+          }}
+        >
+          Draggable Content
+        </motion.div>
+        <div className="profileImg">
+          <img
+            src={require("../assets/profileimage.jpeg")}
+            id="pImg"
+            alt="Profile"
+          />
+          <div className="overlay">
+            <div className="text">Your sentence or two about yourself</div>
+          </div>
         </div>
       </div>
-
-
-      {/* <div class="profileImg">
-        <img src={require("../assets/profileimage.jpeg")} id="pImg" />
-        <div class="overlay">
-          <div class="text">Hello</div>
-        </div>
-      </div> */}
     </div>
   );
 }
