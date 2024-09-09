98b3e90526c428e5fb22556206c98b3544d7b1a2
TNorsang <thenorsang@gmail.com>
2024-09-09 18:45:11 -0400

    about me change

--- src/components/About.js
+++ src/components/About.js
@@ -4,23 +4,63 @@ import "font-awesome/css/font-awesome.min.css";
 import { motion } from "framer-motion";
 import NavBar from "./NavBar";
 import Logo from "./Logo";
+import { useState, useEffect } from "react";
+import { text } from "@fortawesome/fontawesome-svg-core";
 
 export default function About() {
+  const texts = ["Tashi Delek", "Hello", "Namaste"];
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % texts.length);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const textVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: { duration: 0.4 },
+    },
+    exit: {
+      opacity: 0,
+      transition: { duration: 0.4 },
+    },
+  };
+
   return (
     <motion.div className="Background flex justify-center items-center h-screen relative bg-backgroundBlue">
       <motion.div className="w-full absolute top-8 flex justify-center">
         <Logo />
       </motion.div>
 
-      <motion.div className="text-white w-5/6">
-        <h1 className="font-extrabold text-[22px]">Tashi Delek</h1>
-        My name is Norsang Nyandak, and I recently graduated with a Bachelor’s
-        Degree in Computer Science from the New Jersey Institute of Technology.
-        My passion lies in solving real-world problems, and discovering software
-        engineering has shown me how technology can transform my ideas into
-        impactful solutions. Currently, I'm developing Philo, an Artificial
-        Emotional Platform designed to emulate companionship and contribute to
-        improving lives.
+      <motion.div className="text-white w-1/2">
+        <motion.div
+          key={index}
+          initial="hidden"
+          animate="visible"
+          exit="exit"
+          variants={textVariants}
+          className="font-extrabold text-[22px]"
+        >
+          {texts[index]}
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 4, delay: 2 }}
+        >
+          My name is Norsang Nyandak, and I recently graduated with a Bachelor’s
+          Degree in Computer Science from the New Jersey Institute of
+          Technology. My passion lies in solving real-world problems, and
+          discovering software engineering has shown me how technology can
+          transform my ideas into impactful solutions. Currently, I'm developing
+          Philo, an Artificial Emotional Platform designed to emulate
+          companionship and contribute to improving lives.
+        </motion.div>
       </motion.div>
       <NavBar className="text-white flex flex-row space-x-4 fixed bottom-4" />
     </motion.div>
