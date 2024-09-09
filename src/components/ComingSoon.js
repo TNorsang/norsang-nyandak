8ab9289780ae69f14344afe26b8c75c1b98c0466
TNorsang <thenorsang@gmail.com>
2024-09-09 19:44:17 -0400

    spacing update for comingsoon

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -3,8 +3,8 @@ import { motion } from "framer-motion";
 export default function ComingSoon() {
   return (
     <motion.div>
-      <motion.h1 className="text-3xl font-bold text-textBlue">
-        Norsang is Currently Fixing This Page. Please check back later!
+      <motion.h1 className="sm:w-full text-1xl sm:text-3xl font-bold text-textBlue">
+        Norsang is Currently Fixing This Page. <br /> Please check back later!
       </motion.h1>
     </motion.div>
   );
