f1f42e9cbd2acb19ae63a50b2c86b427ddfb1502
Norsang Nyandak <TNorsang>
2024-09-12 14:57:09 -0400

    animation update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,23 @@
-// Importing music
 import "font-awesome/css/font-awesome.min.css";
 import { motion } from "framer-motion";
 import Logo from "./Logo";
 import ProjectCard from "./ProjectCard";
+
+// Variants for staggered animation
+const containerVariants = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.3, // Controls the delay between the appearance of each card
+    },
+  },
+};
+
+const cardVariants = {
+  hidden: { opacity: 0, x: 50 },
+  show: { opacity: 1, x: 0, transition: { duration: 1.5 } }, // Cards appear one by one
+};
 
 export default function Projects() {
   const projects = [
@@ -49,17 +64,25 @@ export default function Projects() {
       <motion.div className="w-1/6 absolute top-8">
         <Logo />
       </motion.div>
-      <motion.div className="container grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-10 top-40 pt-40 pb-40">
+
+      {/* Parent container with staggered animation */}
+      <motion.div
+        className="container grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-10 top-40 pt-40 pb-40"
+        variants={containerVariants} // Applying stagger animation
+        initial="hidden"
+        animate="show"
+      >
         {projects.map((project) => (
-          <ProjectCard
-            key={project.id}
-            title={project.title}
-            description={project.description}
-            link={project.link}
-            linkText={project.linkText}
-            image={project.image}
-            technologies={project.technologies}
-          />
+          <motion.div key={project.id} variants={cardVariants}>
+            <ProjectCard
+              title={project.title}
+              description={project.description}
+              link={project.link}
+              linkText={project.linkText}
+              image={project.image}
+              technologies={project.technologies}
+            />
+          </motion.div>
         ))}
       </motion.div>
     </motion.div>
