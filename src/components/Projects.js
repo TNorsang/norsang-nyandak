72e1b4f86592deabf9b8d0950e7e4cf7644da4f8
Norsang Nyandak <thenorsang@gmail.com>
2025-11-04 10:13:38 -0500

    added desscription to tablet project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -24,7 +24,7 @@ export default function Projects() {
     {
       id: 1,
       title: "Tablet",
-      description: "Luxury eyewear website",
+      description: "Digital Pharmacy",
       link: "https://tablethealth.com/",
       linkText: "View Tablet",
       image: "../images/Tablet.png",
